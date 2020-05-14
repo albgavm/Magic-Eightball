c051cb5aeb66c78644d2772d3c66c20e5824d185
albgavm <albgavm@gmail.com>
2020-05-14 17:43:45 -0400

    >> added mysql add line query

--- Connection test.py
+++ Connection test.py
@@ -2,9 +2,8 @@
 
 
 import mysql.connector
-from Magic Eight Ball import selectrandom
 
-
+import random
 
 mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="H0vhoy2zc4pz", database="eightballtest")
 
@@ -12,19 +11,28 @@ mycursor = mydb.cursor()
 '''
 mycursor.execute("show tables")
 for i in mycursor:
-    print(i)
 
-'''
 
 
 
 #variable = mycursor.execute( "SELECT test FROM Confirm")
-variable = mycursor.execute ("SELECT * FROM Confirm
-    ORDER BY RAND()
-    LIMIT 1;")
-
+variable = mycursor.execute ("SELECT test FROM Confirm ORDER BY RAND() LIMIT 1")
 for i in mycursor:
     print(i)
+'''
 
 
+table = ["1","2","3"]
+x = random.choice(table)
+
+print(x)
+
+if x == "1":
+    mycursor.execute("SELECT test FROM Confirm ORDER BY RAND() LIMIT 1")
+elif x == "2":
+    mycursor.execute("SELECT test FROM Disprove ORDER BY RAND() LIMIT 1")
+elif x == "3":
+    mycursor.execute("SELECT test FROM Notsure ORDER BY RAND() LIMIT 1")
+
+print(mycursor)
 
--- Magic Eight Ball SQL.py
+++ Magic Eight Ball SQL.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import random
 mydb = mysql.connector.connect(host="127.0.0.1", user="root", passwd="H0vhoy2zc4pz", database="eightballtest")
 
 mycursor = mydb.cursor()
@@ -15,12 +16,18 @@ def mergelist(a, b, c):
     #PROBABLY HARD TO MERGE
     return merge
 
-#select random column
+#select random column #MUST FIX does not work
 def selectrandom(listname):
     #just SELECT RANDOM TABLE AND SELECT RANDOM CHOICE
-    draw = random.choice(listname)
-    mycursor.execute("")
-    return (draw)
+    tablelist = (1,2,3)
+    x = random.choice(tablelist)
+    if x == 1:
+        draw = mycursor.execute("SELECT test FROM Confirm ORDER BY RAND() LIMIT 1")
+    elif x == 2:
+        draw = mycursor.execute("SELECT test FROM Disprove ORDER BY RAND() LIMIT 1")
+    elif x == 3:
+        draw = mycursor.execute("SELECT test FROM Notsure ORDER BY RAND() LIMIT 1")
+    return (draw) # <<< --- does not show draw shows only NONE
 
 #showdraw
 def showdraw(x):
@@ -45,20 +52,20 @@ def addline():
     addquestion = input ("would you like to add a text selection panel?")
     if addquestion if ans in ['y', 'Y', 'yes', 'Yes', 'YES']:
         ans = input("where would you like to add a text? (Yes, No or Tryagain tables?)")
-            if ans in ['y', 'Y', 'yes', 'Yes', 'YES']:
-                yesinput = input("Enter text do you want to add as Yes table")
-                mycursor.execute("INSERT INTO Confirn VALUES (?)",(yesinput)) #need to check if it populates the table
-                print("yes statement successfully added")
-            elif ans in ['n', 'N', 'no', 'No', 'NO']:
-                noinput = input("Enter text do you want to add as No table")
-                mycursor.execute("INSERT INTO Disprove VALUES (?)",(noinput))
-                print("no statement successfully added")
-            elif ans in ['tryagain', 'Tryagain', 'try again', 'Try again', 'Try Again', 'TRY AGAIN' ]:
-                tryagaininput = input("Enter text do you want to add as Tryagain table")
-                mycursor.execute("INSERT INTO Notsure VALUES (?)", (tryagaininput))
-                print("no statement successfully added")
-            else:
-                selectagain()
+        if ans in ['y', 'Y', 'yes', 'Yes', 'YES']:
+            yesinput = input("Enter text do you want to add as Yes table")
+            mycursor.execute("INSERT INTO Confirn VALUES (?)",(yesinput)) #need to check if it populates the table
+            print("yes statement successfully added")
+        elif ans in ['n', 'N', 'no', 'No', 'NO']:
+            noinput = input("Enter text do you want to add as No table")
+            mycursor.execute("INSERT INTO Disprove VALUES (?)",(noinput))
+            print("no statement successfully added")
+        elif ans in ['tryagain', 'Tryagain', 'try again', 'Try again', 'Try Again', 'TRY AGAIN' ]:
+            tryagaininput = input("Enter text do you want to add as Tryagain table")
+            mycursor.execute("INSERT INTO Notsure VALUES (?)", (tryagaininput))
+            print("no-statement successfully added")
+        else:
+            selectagain()
 
 
 #def main() need to change first draw from the database
